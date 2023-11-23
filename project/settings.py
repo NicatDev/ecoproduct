b9c8ef475c493c39678bdfb15c7f4ba668f76fa9
NicatDev <nicat254memmedov@gmail.com>
2023-11-24 01:42:04 +0400

    22

--- project/settings.py
+++ project/settings.py
@@ -136,10 +136,10 @@ USE_L10N = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
 
